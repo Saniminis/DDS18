83178853c1970922a9dab7cc0be27a75c9322dd1
SAMUEL SANTIAGO LOUREIRO <sloureiro@labcetec.net>
2024-08-21 16:56:26 -0300

    JavaScript fundamemtos

---
+++ aula9js/script.js
@@ -0,0 +1,145 @@
+//alert("UM GREGORY SELVAGEM APARECEU ! ! !")
+//Mostra um alerta na página
+
+/*------------------------------------
+
+//CNSOLE
+console.log("OLHA UM GREGORY NOVO")
+
+
+var texto = "Eae, Gregory"
+var numero = 1940
+var theBest = true
+var nulo = null
+var indefinido 
+
+//console.log(texto, numero, theBest, nulo, indefinido)
+
+console.log(typeof(texto))
+console.log(typeof(numero))
+console.log(typeof(theBest))
+console.log(typeof(nulo))
+console.log(typeof(indefinido))
+
+
+
+//OPERADORES:
+
+//Aritmétivos: +-/*%
+var a = 10
+var b = 3
+
+console.log("SOMA",a+b)
+console.log("SUBTRAÇÃO",a-b)
+console.log("MULTIPLICAÇÃO",a*b)
+console.log("DIVISÃO",a/b)
+console.log("MÓDULO",a%b)
+
+
+//Lógicos: &&, ||, !
+var verdade = true
+var mentira = false
+
+console.log(verdade && mentira)
+console.log(verdade || mentira)
+console.log(verdade && !mentira)
+
+//Relacionais: >,<,<=,>=,==,!=,===,!==
+var a = 10
+var b = 5
+var c = "10"
+
+console.log(a > b)
+console.log(a < b)
+console.log(a >= b)
+console.log(a <= b)
+console.log(a == b)
+console.log(a != b)
+console.log(a === c)
+console.log(a !== c)
+
+
+
+//Condicionais
+//If
+var estaVivo = null
+console.log(estaVivo)
+if (estaVivo){
+    console.log("Está vivo")
+}
+
+else if(estaVivo == true){
+    onsole.log("Está morto")
+}
+
+else{
+    console.log("Não tá nada")
+}
+
+//Switch
+var camisa = "Preta"
+switch(camisa){
+    case"Azul":
+        console.log("Acamisa é azul")
+    break
+
+    case"Amarela":
+        console.log("Acamisa é amarela")
+    break
+
+    case"Preta":
+        console.log("Acamisa é preta")
+    break
+default:
+    console.log("Acamisa é ???")
+}
+
+
+
+//Loops
+//For
+for(var i = 0; i < 5; i++){
+    console.log("Eu estou aki");
+}
+    console.log("Acabou o FOR")
+
+//While
+var it = 1 
+while (it < 10){
+    console.log("Estou aki agora");
+    console.log(it);
+    it++
+}
+
+
+
+function teste1(){
+    console.log("função1");
+}
+
+function teste2(){
+    console.log("função2");
+}
+
+teste1()
+
+
+
+//Integração com o usuário via prompt
+var resposta = prompt("Digite aki")
+var resposta2 = prompt("Digite aki novamente")
+    console.log(resposta, resposta2)
+
+alert(resposta, resposta2)
+
+------------------------------------*/
+
+
+var numero1 = prompt("digite um numero");
+var numero2 = prompt("digite outro");
+
+var resultado = Number(numero1) + Number(numero2);
+
+console.log(resultado);
+
+alert( resultado);
